d44b783b5ae5e7937aac066eeb169535126980bd
Unknown <unknown@example.com>
2020-03-13 22:40:56 +0400

    Some changes

--- src/app/interfaceComponents/ConnectedElement.tsx
+++ src/app/interfaceComponents/ConnectedElement.tsx
@@ -20,18 +20,23 @@ export const ConnectedElement = ({
   const focusedField = useSelector(
     (state: IRootState) => state.panel.focusedField,
   )
+  const isInitialized = Boolean(
+    useSelector((state: IRootState) => state.panel.pages[page][name]),
+  )
   const dispatch = useDispatch()
   const initComponent = useCallback(() => {
     dispatch(panelActions.initComponent({ page, name, type }))
   }, [dispatch, name, page, type])
 
   useEffect(() => {
-    initComponent()
-  }, [initComponent])
+    if (!isInitialized) {
+      initComponent()
+    }
+  }, [initComponent, isInitialized])
 
   if (focusedField && focusedField === name) {
     // prikrutit' popap
-    return <span style={{ outline: '5px solid yellow' }}>{children}</span>
+    return <span style={{ outline: '3px solid #96c8da' }}>{children}</span>
   }
 
   return <Popup content={name} trigger={children} />
