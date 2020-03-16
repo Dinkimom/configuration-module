b5b2b58b9f635f17a5b615b3545fe304deafe0bb
Nikita <nikitadmitriev97@mail.ru>
2020-03-16 18:03:17 +0400

    Implemented some aplication api methods

--- backend/src/controllers/ExampleController.ts
+++ backend/src/controllers/ExampleController.ts
@@ -1,44 +1,99 @@
-import { Request, Response } from 'express'
 import {
   Controller,
-  Middleware,
-  Get,
-  Put,
-  Post,
   Delete,
+  Get,
+  Post,
+  Put,
+  Middleware,
 } from '@overnightjs/core'
 import { Logger } from '@overnightjs/logger'
+import { Request, Response } from 'express'
 import { MongoClient } from 'mongodb'
 import { connectionString } from '../../config/db'
+import { check, validationResult } from 'express-validator'
 
-@Controller('api')
+@Controller('api/applications')
 export class ExampleController {
-  @Get(':msg')
-  private getMessage(req: Request, res: Response) {
-    MongoClient.connect(connectionString, (err, database) => {
-      if (err) return console.log(err)
+  @Get('items')
+  private getItems(req: Request, res: Response) {
+    try {
+      MongoClient.connect(connectionString, (err, client) => {
+        if (err) throw err
 
-      Logger.Info(req.params.msg)
-      res.status(200).json({
-        message: req.params.msg,
+        const db = client.db('ConfigurationModule')
+        const collection = db.collection('Applications')
+
+        collection.find().toArray((err, result) => {
+          if (err) throw err
+
+          return res.status(200).json({
+            items: result,
+          })
+        })
       })
-    })
+    } catch (err) {
+      return res.status(400).json({ err })
+    }
   }
 
-  @Put(':msg')
-  private putMessage(req: Request, res: Response) {
-    Logger.Info(req.params.msg)
-    return res.status(400).json({
-      error: req.params.msg,
-    })
+  @Get('item/:_id')
+  private getItem(req: Request, res: Response) {
+    try {
+      MongoClient.connect(connectionString, (err, client) => {
+        if (err) throw err
+
+        const db = client.db('ConfigurationModule')
+        const collection = db.collection('Applications')
+
+        collection.findOne({ _id: req.params._id }, (err, result) => {
+          if (err) throw err
+
+          return res.status(200).json({
+            item: result,
+          })
+        })
+      })
+    } catch (err) {
+      return res.status(400).json({ err })
+    }
   }
 
-  @Post(':msg')
-  private postMessage(req: Request, res: Response) {
-    Logger.Info(req.params.msg)
-    return res.status(400).json({
-      error: req.params.msg,
-    })
+  @Post('item')
+  @Middleware([
+    check('name', 'Name is a required field')
+      .not()
+      .isEmpty(),
+    check('descriptionCode')
+      .not()
+      .isEmpty(),
+    check('defaultSettings')
+      .not()
+      .isEmpty(),
+  ])
+  private addItem(req: Request, res: Response) {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    }
+
+    try {
+      MongoClient.connect(connectionString, (err, client) => {
+        if (err) throw err
+
+        const db = client.db('ConfigurationModule')
+        const collection = db.collection('Applications')
+
+        collection.insertOne({ ...req.body }, (err, result) => {
+          if (err) throw err
+
+          return res.status(200).json({
+            item: result,
+          })
+        })
+      })
+    } catch (err) {
+      return res.status(400).json({ err })
+    }
   }
 
   @Delete(':msg')
