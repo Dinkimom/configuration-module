bd91b6cdbd29d5485fd079fcf1861e78090f5b9d
Nikita <nikitadmitriev97@mail.ru>
2020-06-16 23:16:25 +0400

    Fixed url error

--- src/app/EditorModal/saga.ts
+++ src/app/EditorModal/saga.ts
@@ -1,72 +1,74 @@
-import { put, takeEvery } from 'redux-saga/effects'
-import { safeSagaExecute } from '../../middleware/saga'
-import { ApplicationsClient } from '../../services/ApplicationsClient'
-import { IApplicationDTO } from '../../shared/types/IApplicationDTO'
-import { IActionPayloaded } from '../../store/IAction'
-import { notificationSystem } from '../app'
-import { codeEditorActions } from '../CodeEditor/actions'
+import { put, takeEvery } from 'redux-saga/effects';
+import { safeSagaExecute } from '../../middleware/saga';
+import { ApplicationsClient } from '../../services/ApplicationsClient';
+import { IApplicationDTO } from '../../shared/types/IApplicationDTO';
+import { IActionPayloaded } from '../../store/IAction';
+import { notificationSystem } from '../app';
+import { codeEditorActions } from '../CodeEditor/actions';
 import {
-  editorModalActions,
-  EDITOR_MODAL_ADD,
-  EDITOR_MODAL_UPDATE,
-} from './actions'
+	editorModalActions,
+	EDITOR_MODAL_ADD,
+	EDITOR_MODAL_UPDATE,
+} from './actions';
 
-const client = new ApplicationsClient()
+const client = new ApplicationsClient();
 
 export class EditorModalApiSaga {
-  public constructor() {
-    this.add = this.add.bind(this)
-    this.update = this.update.bind(this)
-  }
+	public constructor() {
+		this.add = this.add.bind(this);
+		this.update = this.update.bind(this);
+	}
 
-  public static Initialize() {
-    const saga = new EditorModalApiSaga()
-    return saga.watch()
-  }
+	public static Initialize() {
+		const saga = new EditorModalApiSaga();
+		return saga.watch();
+	}
 
-  public *watch() {
-    yield takeEvery(EDITOR_MODAL_ADD, a => safeSagaExecute(a, this.add))
-    yield takeEvery(EDITOR_MODAL_UPDATE, a => safeSagaExecute(a, this.update))
-  }
+	public *watch() {
+		yield takeEvery(EDITOR_MODAL_ADD, (a) => safeSagaExecute(a, this.add));
+		yield takeEvery(EDITOR_MODAL_UPDATE, (a) =>
+			safeSagaExecute(a, this.update),
+		);
+	}
 
-  private *add(action: IActionPayloaded<IApplicationDTO>) {
-    yield put(editorModalActions.setPending({ flag: true }))
+	private *add(action: IActionPayloaded<IApplicationDTO>) {
+		yield put(editorModalActions.setPending({ flag: true }));
 
-    const response = yield client.add(action.payload)
+		const response = yield client.add(action.payload);
 
-    yield put(editorModalActions.setPending({ flag: false }))
+		yield put(editorModalActions.setPending({ flag: false }));
 
-    if ((response).status === 200) {
-      yield put(editorModalActions.closeModal())
-      window.location.href = `/editor/${response.data._id}`
-    } else {
-      yield put(editorModalActions.failure(response.data.error))
-    }
-  }
+		if (response.status === 200) {
+			yield put(editorModalActions.closeModal());
+			window.location.href = `/configuration_module/editor/${response.data._id}`;
+		} else {
+			yield put(editorModalActions.failure(response.data.error));
+		}
+	}
 
-  private *update(action: IActionPayloaded<IApplicationDTO>) {
-    yield put(editorModalActions.setPending({ flag: true }))
+	private *update(action: IActionPayloaded<IApplicationDTO>) {
+		yield put(editorModalActions.setPending({ flag: true }));
 
-    const response = yield client.update(action.payload)
+		const response = yield client.update(action.payload);
 
-    yield put(editorModalActions.setPending({ flag: false }))
+		yield put(editorModalActions.setPending({ flag: false }));
 
-    if ((response).status === 200) {
-      yield put(
-        codeEditorActions.dataLoaded({
-          name: response.data.item.name,
-          descriptionCode: response.data.item.descriptionCode,
-        }),
-      )
-      yield put(editorModalActions.closeModal())
+		if (response.status === 200) {
+			yield put(
+				codeEditorActions.dataLoaded({
+					name: response.data.item.name,
+					descriptionCode: response.data.item.descriptionCode,
+				}),
+			);
+			yield put(editorModalActions.closeModal());
 
-      notificationSystem.current.addNotification({
-        message: 'Updated successfully!',
-        level: 'success',
-        position: 'tc',
-      })
-    } else {
-      yield put(editorModalActions.failure(response.data.error))
-    }
-  }
+			notificationSystem.current.addNotification({
+				message: 'Updated successfully!',
+				level: 'success',
+				position: 'tc',
+			});
+		} else {
+			yield put(editorModalActions.failure(response.data.error));
+		}
+	}
 }
--- src/app/Panel/reducer.ts
+++ src/app/Panel/reducer.ts
@@ -1,172 +1,190 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable arrow-parens */
 /* eslint-disable @typescript-eslint/no-magic-numbers */
 /* eslint-disable @typescript-eslint/unbound-method */
-import objectAssignDeep from 'object-assign-deep'
-import { colors } from '../../shared/constants/colors'
-import { sizes } from '../../shared/constants/sizes'
-import { ConfigurationElements } from '../../shared/enums/ConfigurationElements'
-import { IOption } from '../../shared/types/IOption'
-import { IActionPayloaded } from '../../store/IAction'
-import { IReducerPayloaded } from '../../store/IReducer'
+import objectAssignDeep from 'object-assign-deep';
+import { colors } from '../../shared/constants/colors';
+import { sizes } from '../../shared/constants/sizes';
+import { ConfigurationElements } from '../../shared/enums/ConfigurationElements';
+import { IOption } from '../../shared/types/IOption';
+import { IActionPayloaded } from '../../store/IAction';
+import { IReducerPayloaded } from '../../store/IReducer';
 import {
-  PANEL_CLEAR,
-  PANEL_FAILURE,
-  PANEL_INIT_COMPONENT,
-  PANEL_INIT_PAGE,
-  PANEL_SET_CURRENT_PAGE,
-  PANEL_SET_FIELD_VALUE,
-  PANEL_SET_FOCUSED_FIELD,
-  PANEL_SET_MODE,
-  PANEL_SET_PENDING,
-  PANEL_SET_RENDER_ERROR,
-  PANEL_VALIDATED,
-} from './actions'
-import { IPanelState } from './state'
+	PANEL_CLEAR,
+	PANEL_FAILURE,
+	PANEL_INIT_COMPONENT,
+	PANEL_INIT_PAGE,
+	PANEL_SET_CURRENT_PAGE,
+	PANEL_SET_FIELD_VALUE,
+	PANEL_SET_FOCUSED_FIELD,
+	PANEL_SET_MODE,
+	PANEL_SET_PENDING,
+	PANEL_SET_RENDER_ERROR,
+	PANEL_VALIDATED,
+} from './actions';
+import { IPanelState } from './state';
 
 const createInitialState = (): any => ({
-  isInitialized: false,
-  name: '',
-  online: false,
-  currentPage: null,
-  renderError: '',
-  settings: {
-    pages: {},
-    common: {},
-  },
-  focusedField: undefined,
-  descriptionCode: '',
-  error: '',
-  isPending: false,
-})
+	isInitialized: false,
+	name: '',
+	online: false,
+	currentPage: null,
+	renderError: '',
+	settings: {
+		pages: {},
+		common: {},
+	},
+	focusedField: undefined,
+	descriptionCode: '',
+	error: '',
+	isPending: false,
+});
 
+// генерация настроек по умолчанию
 const getDefaultSettings = (
-  type: ConfigurationElements,
-  options: IOption[],
+	type: ConfigurationElements,
+	options: IOption[],
 ): any => {
-  switch (type) {
-    case ConfigurationElements.optional:
-      return false
-    case ConfigurationElements.select:
-      return options[0].value
-    case ConfigurationElements.color:
-      return colors[0].value
-    case ConfigurationElements.size:
-      return sizes[3].value
-  }
-}
+	switch (type) {
+		case ConfigurationElements.optional:
+			return false;
+		case ConfigurationElements.select:
+			return options[0].value;
+		case ConfigurationElements.color:
+			return colors[0].value;
+		case ConfigurationElements.size:
+			return sizes[3].value;
+	}
+};
 
 export class PanelReducer implements IReducerPayloaded<IPanelState> {
-  constructor() {
-    this.reduce = this.reduce.bind(this)
-  }
+	constructor() {
+		this.reduce = this.reduce.bind(this);
+	}
 
-  public static Create(): any {
-    const reducer = new PanelReducer()
-    return reducer.reduce
-  }
+	public static Create(): any {
+		const reducer = new PanelReducer();
+		return reducer.reduce;
+	}
 
-  public reduce(
-    state: IPanelState = createInitialState(),
-    action: IActionPayloaded<any>,
-  ): IPanelState {
-    let newState = { ...state }
+	public reduce(
+		state: IPanelState = createInitialState(),
+		action: IActionPayloaded<any>,
+	): IPanelState {
+		let newState = { ...state };
 
-    switch (action.type) {
-      case PANEL_VALIDATED:
-        newState = objectAssignDeep({}, createInitialState(), action.payload)
-        newState.isInitialized = true
-        break
+		switch (action.type) {
+			case PANEL_VALIDATED:
+				newState = objectAssignDeep(
+					{},
+					createInitialState(),
+					action.payload,
+				);
+				newState.isInitialized = true;
+				break;
 
-      case PANEL_INIT_PAGE:
-        if (!newState.settings.pages[action.payload.name]) {
-          newState.settings.pages[action.payload.name] = {}
-          // eslint-disable-next-line prefer-destructuring
-          newState.currentPage = Object.keys(newState.settings.pages)[0]
-        }
-        break
+			case PANEL_INIT_PAGE:
+				if (!newState.settings.pages[action.payload.name]) {
+					newState.settings.pages[action.payload.name] = {};
+					// eslint-disable-next-line prefer-destructuring
+					newState.currentPage = Object.keys(
+						newState.settings.pages,
+					)[0];
+				}
+				break;
 
-      case PANEL_INIT_COMPONENT:
-        if (!newState.settings.common[action.payload.name]) {
-          newState.settings.common[action.payload.name] = {
-            params: {},
-          }
-        }
+			case PANEL_INIT_COMPONENT:
+				if (!newState.settings.common[action.payload.name]) {
+					newState.settings.common[action.payload.name] = {
+						params: {},
+					};
+				}
 
-        Object.keys(action.payload.params).forEach((paramKey) => {
-          if (!newState.settings.common[action.payload.name].params[paramKey]) {
-            newState.settings.common[action.payload.name].params[paramKey] = {
-              value: getDefaultSettings(
-                action.payload.params[paramKey].type,
-                action.payload.params[paramKey].options,
-              ),
-            }
-          }
-        })
+				Object.keys(action.payload.params).forEach((paramKey) => {
+					if (
+						!newState.settings.common[action.payload.name].params[
+							paramKey
+						]
+					) {
+						newState.settings.common[action.payload.name].params[
+							paramKey
+						] = {
+							value: getDefaultSettings(
+								action.payload.params[paramKey].type,
+								action.payload.params[paramKey].options,
+							),
+						};
+					}
+				});
 
-        if (
-          !newState.settings.pages[action.payload.page][action.payload.name]
-        ) {
-          newState.settings.pages[action.payload.page][action.payload.name] = {
-            params: {},
-            common: action.payload.common,
-          }
+				if (
+					!newState.settings.pages[action.payload.page][
+						action.payload.name
+					]
+				) {
+					newState.settings.pages[action.payload.page][
+						action.payload.name
+					] = {
+						params: {},
+						common: action.payload.common,
+					};
 
-          Object.keys(action.payload.params).forEach((paramKey) => {
-            newState.settings.pages[action.payload.page][
-              action.payload.name
-            ].params[paramKey] = {
-              value: action.payload.common
-                ? undefined
-                : getDefaultSettings(
-                    action.payload.params[paramKey].type,
-                    action.payload.params[paramKey].options,
-                  ),
-              type: action.payload.params[paramKey].type,
-              options: action.payload.params[paramKey].options,
-            }
-          })
-        }
-        break
+					Object.keys(action.payload.params).forEach((paramKey) => {
+						newState.settings.pages[action.payload.page][
+							action.payload.name
+						].params[paramKey] = {
+							value: action.payload.common
+								? undefined
+								: getDefaultSettings(
+										action.payload.params[paramKey].type,
+										action.payload.params[paramKey].options,
+								  ),
+							type: action.payload.params[paramKey].type,
+							options: action.payload.params[paramKey].options,
+						};
+					});
+				}
+				break;
 
-      case PANEL_SET_CURRENT_PAGE:
-        newState.currentPage = action.payload.name
-        break
+			case PANEL_SET_CURRENT_PAGE:
+				newState.currentPage = action.payload.name;
+				break;
 
-      case PANEL_CLEAR:
-        newState = createInitialState()
-        break
+			case PANEL_CLEAR:
+				newState = createInitialState();
+				break;
 
-      case PANEL_SET_FIELD_VALUE:
-        if (action.payload.common) {
-          newState.settings.common[action.payload.name].params[
-            action.payload.param
-          ].value = action.payload.value
-        } else {
-          newState.settings.pages[action.payload.page][
-            action.payload.name
-          ].params[action.payload.param].value = action.payload.value
-        }
-        break
+			case PANEL_SET_FIELD_VALUE:
+				if (action.payload.common) {
+					newState.settings.common[action.payload.name].params[
+						action.payload.param
+					].value = action.payload.value;
+				} else {
+					newState.settings.pages[action.payload.page][
+						action.payload.name
+					].params[action.payload.param].value = action.payload.value;
+				}
+				break;
 
-      case PANEL_SET_FOCUSED_FIELD:
-        newState.focusedField = action.payload.name
-        break
+			case PANEL_SET_FOCUSED_FIELD:
+				newState.focusedField = action.payload.name;
+				break;
 
-      case PANEL_SET_MODE:
-        newState.online = action.payload.online
-        break
+			case PANEL_SET_MODE:
+				newState.online = action.payload.online;
+				break;
 
-      case PANEL_SET_RENDER_ERROR:
-        newState.renderError = action.payload.error
-        break
-      case PANEL_SET_PENDING:
-        newState.isPending = action.payload.flag
-        break
-      case PANEL_FAILURE:
-        newState.error = action.payload.error
-        break
-    }
+			case PANEL_SET_RENDER_ERROR:
+				newState.renderError = action.payload.error;
+				break;
+			case PANEL_SET_PENDING:
+				newState.isPending = action.payload.flag;
+				break;
+			case PANEL_FAILURE:
+				newState.error = action.payload.error;
+				break;
+		}
 
-    return newState
-  }
+		return newState;
+	}
 }
